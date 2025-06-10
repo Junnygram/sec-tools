bb91489be9aa697dc00fc56d852ca08e119ba91e
Junnygram <you@example.com>
2025-06-10 18:27:02 +0100

    ft

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,18 +106,3 @@ resource "aws_eip" "app_eip" {
   }
 }
 
-# Output the public IP of the EC2 instance
-output "ec2_public_ip" {
-  value = aws_eip.app_eip.public_ip
-  description = "The public IP address of the EC2 instance"
-}
-
-output "frontend_url" {
-  value = "http://${aws_eip.app_eip.public_ip}:3000"
-  description = "URL to access the frontend application"
-}
-
-output "backend_url" {
-  value = "http://${aws_eip.app_eip.public_ip}:8080"
-  description = "URL to access the backend API"
-}
